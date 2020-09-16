5b761b81ac1dc3c1f7163b0d57abf972f8938048
Ankur Seth <madhumeya@gmail.com>
2020-09-17 04:50:56 +0530

    Printing a red t2b gradient

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
+import numpy
 from PIL import Image
 
 
 
 def main():
-	im = Image.new('RGB', (1024, 1024))
-	im.putdata([(255,0,0), (0,255,0), (0,0,255)])
+	data = numpy.zeros((1024, 1024, 3), dtype=numpy.uint8)
+	max_pixel = 1024
+	for x in range(0, max_pixel):
+		for y in range(0, max_pixel):
+			data[x, y] = [x/4, 0, 0]
+			pass
+		pass
+
+	im = Image.fromarray(data)
 	im.save('test.png')
+	pass
 
 
 if __name__ == '__main__':
 	main()
+	pass
