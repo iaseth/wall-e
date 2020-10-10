03f62eee15eb1613b030bf0ecb1d7a496954f4b5
Ankur Seth <iankurseth@gmail.com>
2020-10-10 23:57:37 +0530

    Into chessboards

---
+++ chessboards/main.py
@@ -0,0 +1,18 @@
+import math
+
+import numpy
+from PIL import Image
+
+
+
+def chessboard_stuff():
+	print("Hello, chessboards!")
+	pass
+
+def main():
+	chessboard_stuff()
+
+
+if __name__ == '__main__':
+	main()
+	pass
