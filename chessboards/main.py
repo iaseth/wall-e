61f6ef9864eceebfa7fb7b0f0107c41116567356
Ankur Seth <iankurseth@gmail.com>
2020-10-11 01:13:53 +0530

    Printing chessboards

--- chessboards/main.py
+++ chessboards/main.py
@@ -26,10 +26,25 @@ class Resolution():
 		return f"{self.name} [{self.width}x{self.height}]"
 
 
+class Chessboard():
+	def __init__(self, primary, secondary, resolution):
+		self.primary = primary
+		self.secondary = secondary
+		self.resolution = resolution
+		pass
+
+	def filename(self):
+		return f"{self.primary.name}_{self.secondary.name}_{self.resolution.name}.png"
+
+	def __str__(self):
+		return f"{self.filename()}"
+
+
 class App():
 	def __init__(self):
 		self.setupColors()
 		self.setupResolutions()
+		self.setupChessboards()
 		pass
 
 	def setupColors(self):
@@ -52,6 +67,15 @@ class App():
 			self.resolutions.append(resolution)
 		pass
 
+	def setupChessboards(self):
+		self.chessboards = []
+		for primary in self.colors:
+			for secondary in self.colors:
+				for resolution in self.resolutions:
+					chessboard = Chessboard(primary, secondary, resolution)
+					self.chessboards.append(chessboard)
+		pass
+
 	def printColors(self):
 		for color in self.colors:
 			print(color)
@@ -62,11 +86,17 @@ class App():
 			print(resolution)
 		pass
 
+	def printChessboards(self):
+		x = 0
+		for chessboard in self.chessboards:
+			print(f"{x+1}. {chessboard}")
+			x += 1
+		pass
+
 
 def chessboard_stuff():
 	app = App()
-	app.printColors()
-	app.printResolutions()
+	app.printChessboards()
 	pass
 
 
