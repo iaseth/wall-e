4804486eb1896306530dcbeb7bbde73c4b338405
Ankur Seth <iankurseth@gmail.com>
2020-10-11 11:36:58 +0530

    Moved Chessboard class to pywall

--- app/chessboard.py
+++ app/chessboard.py
@@ -1,4 +1,3 @@
-import os
 import math
 import json
 
@@ -7,61 +6,8 @@ from PIL import Image
 
 from pywall.color import Color
 from pywall.resolution import Resolution
+from pywall.chessboard import Chessboard
 
-
-
-class Chessboard():
-	def __init__(self, primary, secondary, resolution):
-		self.primary = primary
-		self.secondary = secondary
-		self.resolution = resolution
-		pass
-
-	def hasTwoColors(self):
-		return (self.primary != self.secondary)
-
-	def existsOnDisk(self):
-		if os.path.isfile(self.filepath()):
-			return True
-		else:
-			return False
-		pass
-
-	def saveToDisk(self):
-		data = self.resolution.getNumpyArray()
-
-		primary = self.primary.getRGB()
-		secondary = self.secondary.getRGB()
-
-		cell_height = math.ceil(self.resolution.height / 8)
-		cell_width = math.ceil(self.resolution.width / math.floor(self.resolution.width/cell_height))
-
-		for x in range(0, self.resolution.height):
-			for y in range(0, self.resolution.width):
-				cx = math.floor(x / cell_height)
-				cy = math.floor(y / cell_width)
-				cx_even = cx%2 == 0
-				cy_even = cy%2 == 0
-				white = (cx_even and cy_even) or ((not cx_even) and (not cy_even))
-				if white:
-					data[x, y] = primary
-				else:
-					data[x, y] = secondary
-				pass
-			pass
-
-		im = Image.fromarray(data)
-		im.save(self.filepath())
-		pass
-
-	def filepath(self):
-		return f"pngs/chessboards/{self.filename()}"
-
-	def filename(self):
-		return f"chessboard_{self.primary.name}_{self.secondary.name}_{self.resolution.name}.png"
-
-	def __str__(self):
-		return f"({self.primary.name}|{self.secondary.name}) {self.resolution}"
 
 
 class App():
