b4be100672edb7427e2608900c4acabbdfea6d6a
Ankur Seth <iankurseth@gmail.com>
2020-10-14 11:39:34 +0530

    Removed clutter

--- app/generate.py
+++ app/generate.py
@@ -7,20 +7,7 @@ from pywall.walleapp import WallEApp
 
 
 def test_stuff():
-	data = numpy.zeros((1024, 1024, 3), dtype=numpy.uint8)
-	max_pixel = 1024
-	for x in range(0, max_pixel):
-		for y in range(0, max_pixel):
-			red = math.floor((y/4) / 16) * 16
-			green = math.floor((x/4) / 16) * 16
-			data[x, y] = [red, green, 0]
-			pass
-		pass
-
-	im = Image.fromarray(data)
-	im.save('test.png')
 	pass
-
 
 def generate_stuff():
 	app = WallEApp()
